3f66b92efb39738198874ab453be9f3ddc8efd1f
Michal Srb <srb@convex.dev>
2023-03-29 11:15:23 -0700

    Add skeleton composer

--- src/server/api/routers/posts.ts
+++ src/server/api/routers/posts.ts
@@ -1,9 +1,23 @@
+import { clerkClient } from "@clerk/nextjs/server";
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
 export const postsRouter = createTRPCRouter({
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.post.findMany();
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    const posts = await ctx.prisma.post.findMany({ take: 100 });
+    const users = (
+      await clerkClient.users.getUserList({
+        userId: posts.map((post) => post.authorId),
+      })
+    ).map(({ id, username, profileImageUrl }) => ({
+      id,
+      username,
+      profileImageUrl,
+    }));
+    return posts.map((post) => ({
+      ...post,
+      author: users.find((user) => user.id === post.authorId),
+    }));
   }),
 });
